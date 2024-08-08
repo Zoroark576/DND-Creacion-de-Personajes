73495a63fafb56cb414ebda4c117b50a700383ed
Zoruark576 <74374759+Zoroark576@users.noreply.github.com>
2024-08-07 21:45:24 -0700

    Editar Lore
    Ahora se puede editar el lore

--- Scripts/VerPersonaje/VerLoreBotonVolver.gd
+++ Scripts/VerPersonaje/VerLoreBotonVolver.gd
@@ -1,2 +1,14 @@
 extends botonesVolverSiguiente
 
+@export var contDatos : GridContainer
+@export var nNombre : LineEdit
+
+func _on_volver_pressed():
+	for dato in contDatos.get_children():
+		if dato is LineEdit:
+			Personaje.lore[dato.name] = dato.text 
+		elif dato is SpinBox:
+			Personaje.lore[dato.name] = dato.value
+		elif dato is TextEdit:
+			Personaje.lore[dato.name] = dato.text
+	super()
